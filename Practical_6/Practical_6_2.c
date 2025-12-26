40fe985244c4b3ce2c173a4bdf98b0135b1aca71
B Yakshita <shaveemade4u@gmail.com>
2025-12-26 21:26:21 +0530

    6.2
    Added comments for clarity and output example.

--- Practical_6/Practical_6_2.c
+++ Practical_6/Practical_6_2.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main() {
+
+    //B_YAKSHITA_10371
     int limit, sum = 0, i;
 
     printf("Enter the limit: ");
     scanf("%d", &limit);
-
     for (i = 1; i <= limit; i++) {
         if (i % 2 != 0) {
             continue;
         }
         sum += i;
     }
-
     printf("Sum of even numbers from 1 to %d is: %d\n", limit, sum);
-
     return 0;
 }
+//Output
+//Enter the limit: 9
+//Sum of even numbers from 1 to 9 is: 20
